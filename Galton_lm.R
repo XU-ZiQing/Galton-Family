8daf2d4e1591d34f68c09e43a6cbf824fd2c0b77
XU-ZiQing <49932753+XU-ZiQing@users.noreply.github.com>
2019-05-09 16:52:23 +0800

    Add files via upload

---
+++ Galton_lm.R
@@ -0,0 +1,23 @@
+library("HistData")
+library("ridge")
+set.seed(1)
+attach(GaltonFamilies)
+str(GaltonFamilies)
+inputData <- data.frame(GaltonFamilies)
+colnames(inputData)[8]<-"response"
+colnames(inputData)[6]<-"gender"
+inputData[ ,6] <- as.numeric(inputData[,7]=="male")
+inputData <- inputData[,-c(1,7)]
+mse<-c()
+for (tmp in 1:50){
+trainingIndex <- sample(1:nrow(inputData),nrow(inputData)-200)
+trainingData <- inputData[trainingIndex, ]
+testData <- inputData[-trainingIndex, ]
+RidMod <- lm(response ~ ., data = trainingData)
+predicted <- predict(RidMod, testData)
+compare <- cbind(actual = testData$response, predicted)
+mse[tmp] <- sum((compare[,1]-compare[,2])^2)/200
+}
+mse
+sqrt(mean(mse))
+var(mse)
